7c149fd1174a8cd345fe7f8ae443f53d04423ed3
Roby Raju Oommen <31279570+robyrajuoommen@users.noreply.github.com>
2019-01-29 22:58:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<!-- <h2 align="center"><u>EMPLOYEE PORTAL</u></h2> -->
+<table align="center" style="margin-top: 20%;">
+	<tr>
+		<td><a href="adminlogin.php"><button type="submit" name="admlog" style="margin-right:200px;width:220px;height:100px;"><img src="admlog.png" width="60px" height="60px"><center>ADMIN LOGIN</center></button></a></td>
+		<td><a href="userlogin.php"><button type="submit" name="usrlog" style="padding-top:10px;width:220px;height:100px;"><span><img src="usrlog.png" width="50px" height="50px"><center>USER LOGIN</center></span></button></a></td>
+	</tr>
+</table>
+</body>
+</html>
